ead3f20e92b6c8a4f038c8eb3bebe5d582124241
Nikita Fuchs <q.fuchs@gmail.com>
2019-09-14 09:46:04 +0200

    create backup of modified logger npm package in project's root

---
+++ ngx-log-monitor/index.d.ts
@@ -0,0 +1,3 @@
+export * from './lib/log-monitor.component';
+export * from './lib/log-monitor.module';
+export * from './lib/models/log-message.model';
---
+++ ngx-log-monitor/lib/helpers/log-message.helper.d.ts
@@ -0,0 +1,2 @@
+import { LogMessage } from '../models/log-message.model';
+export declare const normalizeLogMessage: (msg: LogMessage) => LogMessage;
