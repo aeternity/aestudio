7ec73a3c1daa3749710e7449d6f79f69c7374219
Nikita Fuchs <q.fuchs@gmail.com>
2020-06-03 13:20:24 +0200

    create new env file, add deployment options for GH-pages,

--- inject-metadata.js
+++ inject-metadata.js
@@ -1,12 +1,13 @@
 const insertLine = require('insert-line');
 const fs = require('fs');
+import { environment } from './environments/environment';
 
-const metaTags= `<meta property="og:title" content="Sophia Fire Editor">
+const metaTags= `<meta property="og:title" content="AE Studio">
 <meta property="og:site_name" content="Nikita Fuchs">
-<meta property="og:url" content="http://fireeditor.nikitafuchs.de">
+<meta property="og:url" content="${environment.appUrl}">
 <meta property="og:description" content="Contract editor that does the work for you, not the other way round.">
 <meta property="og:type" content="product">
-<meta property="og:image" content="http://fireeditor.nikitafuchs.de/assets/preview.png">
+<meta property="og:image" content="${environment.appUrl}">
 `
 
 insertLine('./dist/fire-editor/index.html').content(metaTags).at(5).then(function(err) {
