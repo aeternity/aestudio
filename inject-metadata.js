a19c89b1f24793109c2ba264734ee59a615de498
Nikita Fuchs <q.fuchs@gmail.com>
2021-10-12 14:15:54 +0200

    remove typescript-require dep by manually parsing env file

--- inject-metadata.js
+++ inject-metadata.js
@@ -2,15 +2,17 @@
 
 const insertLine = require('insert-line');
 const fs = require('fs');
-require('typescript-require');
-const environment = require('./src/environments/environment.ae.ts')
+const fileContent = fs.readFileSync('./src/environments/environment.ae.ts', 'utf-8');
+const jsonRegex = new RegExp(`{([^}]*})}`, 'gm')
+const environmentString = fileContent.match(jsonRegex)[0]
+const environment = JSON.parse(environmentString)
 
 const metaTags= `<meta property="og:title" content="AE Studio">
 <meta property="og:site_name" content="AE Studio">
-<meta property="og:url" content="${environment.environment.appUrl}">
+<meta property="og:url" content="${environment.appUrl}">
 <meta property="og:description" content="Contract editor that does the work for you, not the other way round.">
 <meta property="og:type" content="product">
-<meta property="og:image" content="${environment.environment.appUrl}/assets/preview.png">
+<meta property="og:image" content="${environment.appUrl}/assets/preview.png">
 `
 
 insertLine('./dist/index.html').content(metaTags).at(5).then(function(err) {
--- src/environments/environment.ae.ts
+++ src/environments/environment.ae.ts
@@ -1,20 +1,19 @@
 export const environment = {
-  production: true,
-  contractSharingBackend: 'https://xfs2awe868.execute-api.eu-central-1.amazonaws.com/dev/candidates/',
-  appUrl: 'https://studio.aepps.com',
-  testnetFaucetUrl: 'https://testnet.faucet.aepps.com/account/',
-  compilerURL:'https://latest.compiler.aepps.com',
-  debugCompilerURL : 'https://latest.compiler.aepps.com',
-  publicTestnetURL : 'https://testnet.aeternity.io',
-  compilerRequestDelay : 400,
-  mainNetExplorer: 'https://explorer.aeternity.io',
-  testNetExplorer: 'https://explorer.testnet.aeternity.io',
-  firebase: {
-    apiKey: "AIzaSyD2uvXdg_TBcXlvj_AxXUtwsSqfgk3sT7k",
-    authDomain: "fire-editor-f1e92.firebaseapp.com",
-    databaseURL: "https://fire-editor-f1e92.firebaseio.com",
-    projectId: "fire-editor-f1e92",
-    messagingSenderId: "522100874686",
-    appId: "1:522100874686:web:9f854fa36991f016066def"
-  }
-};
+  "production": true,
+  "contractSharingBackend": "https://xfs2awe868.execute-api.eu-central-1.amazonaws.com/dev/candidates/",
+  "appUrl": "https://studio.aepps.com",
+  "testnetFaucetUrl": "https://testnet.faucet.aepps.com/account/",
+  "compilerURL":"https://latest.compiler.aepps.com",
+  "debugCompilerURL" : "https://latest.compiler.aepps.com",
+  "publicTestnetURL" : "https://testnet.aeternity.io",
+  "compilerRequestDelay" : 400,
+  "mainNetExplorer": "https://explorer.aeternity.io",
+  "testNetExplorer": "https://explorer.testnet.aeternity.io",
+  "firebase": {
+    "apiKey": "AIzaSyD2uvXdg_TBcXlvj_AxXUtwsSqfgk3sT7k",
+    "authDomain": "fire-editor-f1e92.firebaseapp.com",
+    "databaseURL": "https://fire-editor-f1e92.firebaseio.com",
+    "projectId": "fire-editor-f1e92",
+    "messagingSenderId": "522100874686",
+    "appId": "1:522100874686:web:9f854fa36991f016066def"
+  }}
